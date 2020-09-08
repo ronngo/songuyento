fa927b7d3e29039b170ff0a674c17dae6cd78307
ronngo <ronngomn23@gmail.com>
2020-09-08 11:52:52 +0700

    songuyento
    songuyento

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int n = 0;
+            Console.Write("Nhap mot so bat ky: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+                if (number % i == 0)
+                    n++;
+            if (n == 2)
+                Console.WriteLine("{0} la so nguyen to.", number);
+            else
+                Console.WriteLine("{0} khong phai la so nguyen to.", number);
+            Console.ReadKey();
+        }
+    }
+}
